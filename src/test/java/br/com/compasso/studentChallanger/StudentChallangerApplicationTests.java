b5e7204f55dadcdf76312c60ba336b1f4266e155
matheus <matheusbvieira@hotmail.com>
2020-11-13 13:46:02 -0300

    Commit Inicial

---
+++ src/test/java/br/com/compasso/studentChallanger/StudentChallangerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compasso.studentChallanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentChallangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
